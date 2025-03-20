f9dea4e1953513f497c81bf21c44de90aeadb32e
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-03-20 20:33:36 -0300

    add function to find user by slug and format avatar URLs

--- src/services/user.ts
+++ src/services/user.ts
@@ -17,3 +17,29 @@ export const findUserByEmail = async (email: string) => {
 
     return null;
 };
+
+export const findUserBySlug = async (slug: string) => {
+    const user = await prisma.user.findFirst({
+        select: {
+            avatar: true,
+            cover: true,
+            slug: true,
+            name: true,
+            bio: true,
+            link: true,
+        },
+        where: {
+            slug,
+        },
+    });
+
+    if (user) {
+        return {
+            ...user,
+            avatar: getPublicURL(user.avatar),
+            cover: getPublicURL(user.cover),
+        };
+    }
+
+    return null;
+};
