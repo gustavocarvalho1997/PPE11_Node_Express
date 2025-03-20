e395a1e9b2f9b374885fbf096218b8094f8b36ea
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-03-20 19:55:05 -0300

    feita a validacao dos dados recebidos na requisicao de signup

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,3 +1,20 @@
 import type { Request, Response } from 'express';
+import { signupSchema } from '../schemas/signup';
 
-export const signup = async (req: Request, res: Response) => {};
+export const signup = async (request: Request, response: Response) => {
+    // Validar os dados recebidos
+    const safeData = signupSchema.safeParse(request.body);
+    if (!safeData.success) {
+        response
+            .status(400)
+            .json({ error: safeData.error.flatten().fieldErrors });
+        return;
+    }
+    // verificar email
+    // verificar slug
+    // gerar hash da senha
+    // cria o usuário
+    // cria o token
+    // retorna o resultado {token, user}
+    response.json({});
+};
