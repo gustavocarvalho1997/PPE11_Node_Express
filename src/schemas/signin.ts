91d9ad50ea4bd9878ea6ae0b7b6d4afb6e3ff98d
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2025-03-20 21:41:00 -0300

    add signin schema validation using zod for email and password

---
+++ src/schemas/signin.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+
+export const signinSchema = z.object({
+    email: z
+        .string({ message: 'E-mail é obrigatório' })
+        .email('E-mail inválido'),
+    password: z
+        .string({ message: 'Senha é obrigatória' })
+        .min(4, 'Senha precisa ter no mínimo 4 caracteres'),
+})
